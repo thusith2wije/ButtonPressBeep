eb636b1cdece259d473806470f27e8b1712e6da1
Thusitha Wijerathne <tij@tiqri.com>
2020-03-30 13:26:48 +0530

    Initial commit

---
+++ KeyPressBeep/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyPressBeep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                SoundPlayer sd = new SoundPlayer();
+                sd.SoundLocation = @"D:\TIQRICDM\cdm\Inputs\ButtonClick\button_click_001.wav";
+                sd.Play();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error :- Beep Sound path not found");
+            }
+        }
+    }
+}
